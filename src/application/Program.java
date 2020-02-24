a411733bcdc0edec0243c7987fc3e204d7916647
constantinoi <pescaalma@gmail.com>
2020-02-24 18:36:05 -0400

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,7 +33,10 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+		System.out.println("==== TESTE 3 : seller insert ====");
+		Seller newSeller = new Seller (null, "Greg", "greg@gmail.com", new Date(),4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 		
 	}
 
